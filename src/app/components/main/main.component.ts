1e3fce488b584ea196e853aa04fe1d7c01aca136
evelyne802 <linagluzman@gmail.com>
2025-01-16 23:17:39 +0200

    fix: media queries for mobile view

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [ 
+    MatFormFieldModule, 
+    ReactiveFormsModule, 
+    FormsModule 
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
 
+  roleFormControl = new FormControl('');
+  resumeFormControl = new FormControl('');
+  resumeDisabled = true;
+  resumeBoxClasses = "choose-input";
+
+  removeButtons() {
+    this.resumeDisabled = false;
+    this.resumeBoxClasses = "choose-input-disabled";
+  }
 }
