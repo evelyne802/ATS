5679f8d2b3840dd8d27c21e8db1ea633d38b1e5a
evelyne802 <linagluzman@gmail.com>
2025-01-21 14:55:03 +0200

    feat: file upload

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [ 
     MatFormFieldModule, 
+    MatInputModule,
+    //BrowserAnimationsModule,
     ReactiveFormsModule, 
     FormsModule 
   ],
@@ -16,14 +18,41 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  file: File | undefined;
+  fileName: string = '';
 
   roleFormControl = new FormControl('');
   resumeFormControl = new FormControl('');
   resumeDisabled = true;
-  resumeBoxClasses = "choose-input";
+  resumeBoxClass = "choose-input";
+  resumeFileClass = "disabled-file-name";
+  textAreaClass = "textarea default-cursor"
 
-  removeButtons() {
+  textResume() {
     this.resumeDisabled = false;
-    this.resumeBoxClasses = "choose-input-disabled";
+    this.resumeBoxClass = "choose-input-disabled";
+    this.textAreaClass = "textarea";
+  }
+
+  fileResume() {
+    this.resumeBoxClass = "choose-input-disabled";
+    this.resumeFileClass = 'show-file-name';
+  }
+
+  closeFileUpload() {
+    this.file = undefined;
+    console.log('closeFile', this.file);
+    this.resumeDisabled = true;
+    this.resumeBoxClass = "choose-input";
+    this.resumeFileClass = "disabled-file-name";
+  }
+
+  uploadFile(event: any) {
+    this.file = event.target.files[0];
+    console.log(this.file);
+    if (this.file) {
+      this.fileName = this.file.name;
+      this.fileResume();
+    }
   }
 }
